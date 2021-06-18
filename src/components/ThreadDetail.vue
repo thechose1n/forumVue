9decd32f98052951bcf29dc0430bd0aae7cc540a
Mustafa Can <MustafaCan@Mustafas-MBP.fritz.box>
2021-06-18 14:25:34 +0200

    add feature to get id from url, if reloading for example

--- src/components/ThreadDetail.vue
+++ src/components/ThreadDetail.vue
@@ -1,8 +1,12 @@
 <template>
     <div>
-        <h1>{{ thread.id }}</h1>
-        <h2>{{ thread.title }}</h2>
-        <h3>{{ thread.content }}</h3>
+      <b-card :title=thread.title>
+        <b-card-text>
+          {{ thread.content }}
+        </b-card-text>
+        <b-button class="button" variant="light" v-on:click="goBack()">Zurück</b-button>
+        <b-button class="button" variant="danger" v-on:click="deleteThread(thread.id)">Löschen</b-button>
+      </b-card>
     </div>
 </template>
 
@@ -18,17 +22,31 @@
 
         },
         computed: {
+          //Funktion zur Filterung der Threads anhand des übermittelten Parameters 'id'
             thread() {
                 return store.threads.find(
-                    thread => thread.id === this.threadId
+                    thread => thread.id == this.threadId
                 );
             }
-        }
+        },
+        methods: {
+          deleteThread (id) {
+            store.threads = store.threads.filter(function(obj) {
+              return obj.id !== id;
+            });
+            this.$router.push('/')
+          },
+          goBack() {
+            this.$router.push('/')
+          }
+      }
     }
 </script>
 
 <style scoped>
 
-
+.button {
+  margin-right: 20px;
+}
 
 </style>
