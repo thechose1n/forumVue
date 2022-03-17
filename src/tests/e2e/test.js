6c58db4e65841da13fb184e45f170db4b4cd8090
Mustafa Can <MustafaCan@Mustafas-MBP.fritz.box>
2022-03-17 13:12:59 +0100

    add nightwatch test file

---
+++ src/tests/e2e/test.js
@@ -0,0 +1,17 @@
+describe('thready demo', function() {
+
+    before(browser => browser.navigateTo('http://localhost:8080/'));
+
+    test('demo test thready', function(browser) {
+        browser
+            .waitForElementVisible('body')
+            .assert.titleContains('forumvue')
+            .assert.visible('button.btn.create.btn-primary')
+            .click('button.btn.create.btn-primary')
+            .assert.visible('input#title-input.form-control')
+            .assert.visible('input#content-input.form-control')
+            .assert.visible('button.btn.btn-primary')
+            .setValue('input#title-input.form-control', 'Nightwatch Test')
+            .setValue('input#content-input.form-control', 'Dies ist ein nightwatch Test')
+    });
+});
